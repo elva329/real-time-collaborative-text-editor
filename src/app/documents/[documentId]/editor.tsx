fbc0a9967de96c55d0b4c0c2d78c44db66e01432
elva <jia_shen@126.com>
2025-06-04 15:49:07 +0800

    feat: added image and image-resize extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -4,6 +4,12 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image';
 
 export const Editor = () => {
   const editor = useEditor({
@@ -16,12 +22,35 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Image,
+      ImageResize,
       TaskList,
       TaskItem.configure({
         nested: true
       }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell
     ],
-    content: '<p>Hello World! 🌎️</p>',
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
   })
 
   return (
