4ece2305348f7e4deff79e8c3c45a80ab924060b
elva <jia_shen@126.com>
2025-06-11 16:14:20 +0800

    feat: implemented line-height extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,7 @@ import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
 
 import { FontSizeExtension } from '../../extensions/font-size'
+import { LineHeightExtension } from '../../extensions/line-height'
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -38,6 +39,10 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      LineHeightExtension.configure({
+        types: ['heading', 'paragraph'],
+        defaultLineHeight: 'normal'
+      }),
       FontSizeExtension,
       TextAlign.configure({
         types: ['heading', 'paragraph']
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -11,6 +11,7 @@ import {
   ImageIcon,
   ItalicIcon,
   Link2Icon,
+  ListCollapseIcon,
   ListIcon,
   ListOrderedIcon,
   ListTodoIcon,
@@ -129,6 +130,63 @@ const ImageButton = () => {
         </DialogContent>
       </Dialog>
     </>
+  )
+}
+
+const LineHeightButton = () => {
+  const { editor } = useEditorStore();
+  const lineHeights = [
+    {
+      label: 'Default',
+      value: 'normal',
+    }, {
+      label: 'Single',
+      value: '1',
+    },
+    {
+      label: '1.15',
+      value: '1.15',
+    },
+    {
+      label: '1.5',
+      value: '1.5',
+    },
+    {
+      label: 'Double',
+      value: '2',
+    }
+  ]
+
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button
+          className='h7 min-w-7 shrink-0 
+          flex flex-col items-center justify-center
+          rounded-sm hover:bg-neutral-200/80 px-1.5 
+          overflow-hidden 
+          text-sm'
+        >
+          <ListCollapseIcon className='size-4' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-1 flex flex-col gap-y-1'>
+        {lineHeights.map(({ label, value }) => {
+          return (
+            <button
+              key={value}
+              onClick={() => editor?.chain().focus().setLineHeight(value).run()}
+              className={cn('flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80',
+                editor?.getAttributes('paragraph').lineHeight === value && 'bg-neutral-200/80'
+              )}
+            >
+              <span>{label}</span>
+            </button>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
@@ -691,6 +749,7 @@ const Toolbar = () => {
       <LinkButton />
       <ImageButton />
       <AlignButton />
+      <LineHeightButton />
       <ListButton />
 
       <Separator orientation='vertical' className='bg-neutral-300' />
