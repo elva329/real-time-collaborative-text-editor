28cbeb992951099ef54481cbc10d72ff69635a7f
elva <jia_shen@126.com>
2025-06-17 16:14:34 +0800

    feat: added search input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
 import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: 'Next.js',
@@ -12,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Home() {
-  return (
-    <div className='flex min-h-screen items-center justify-center'>
-      Click <Link href='/documents/123'><span className='text underline'></span>&nbsp;Here</Link> to go to document ID
-    </div>
-  );
-} 
