86264d4884592200a33ccae9de7d3f761aafc58f
elva <jia_shen@126.com>
2025-07-02 17:11:35 +0800

    feat: updated Schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,5 +5,15 @@ import { v } from "convex/values";
 export default defineSchema({
   documents: defineTable({
     title: v.string(),
-  }),
+    initialContent: v.optional(v.string()),
+    ownerId: v.string(),
+    roomId: v.string(),
+    organizationId: v.optional(v.string())
+  })
+  .index('by_owner_id', ['ownerId'])
+  .index('organization_id', ['organizationId'])
+  .searchIndex('search_title', {
+    searchField: 'title',
+    filterFields: ['ownerId', 'organizationId']
+  })
 });
